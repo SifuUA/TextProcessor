530e0b39b6e49bfcc80312a04d517ae5a7a54049
Oleksandr Arzamastsev <sarzamastsev5@gmail.com>
2020-02-01 22:22:24 +0200

    Implement a skeleton for mapping text to uppercase

--- src/test/java/view/ConsoleReaderTest.java
+++ src/test/java/view/ConsoleReaderTest.java
@@ -1,10 +1,11 @@
 package view;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ConsoleReaderTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void readInput() {
     }
+
 }
